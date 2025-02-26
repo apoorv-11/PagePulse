90811bdebaae863c7a5aab321de6a62e146d3761
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-02-26 09:27:40 +0530

    Middleware Setup

---
+++ Backend/src/Middlewwares/verifyToken.middleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const verifyAdminToken = (req, res, next) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access Denied", error: true });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res
+        .status(401)
+        .json({ message: "Invalid Credentials", error: true });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+export default verifyAdminToken;
