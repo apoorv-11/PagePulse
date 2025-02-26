0b70ae7a64d8e4c2fb12a4d0378b7e4f60cb4eeb
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-02-26 17:47:38 +0530

    Backend Deployment

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://page-pulse-mjh7sf0ap-nevin100s-projects.vercel.app",
+    ],
     credentials: true,
   })
 );
