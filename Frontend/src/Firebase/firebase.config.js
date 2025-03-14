7a757ea2adb0029c818e2c3be0fbbbdc42770654
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-03-14 12:08:21 +0530

    Completed Project

--- Frontend/src/Firebase/firebase.config.js
+++ Frontend/src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5hNdE6ia4IYvbyuwrvxAsHDr4rKGCako",
-  authDomain: "page-pulse--mern--app.firebaseapp.com",
-  projectId: "page-pulse--mern--app",
-  storageBucket: "page-pulse--mern--app.firebasestorage.app",
-  messagingSenderId: "686392809493",
-  appId: "1:686392809493:web:237c012967ea7d6048961c",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
